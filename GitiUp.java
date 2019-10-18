383ffd091ce2ecf424b374ba27ecfb9333d2624f
TigerdaddyCat <senakawahama@gmail.com>
2019-10-18 16:31:31 -0400

    added 'cube'

--- GitiUp.java
+++ GitiUp.java
@@ -1,15 +1,18 @@
 public class GitiUp {
 	public void xd(){
 		int lmao;
-		for(lmao = 4; lmao > 0; lmao--){
+		for(lmao = 4; lmao > 0; lmao--)
+        {
 			System.out.print("kek ");	
-		}System.out.println("");
+		}
+        System.out.println("");
 	}
 	
 	public static void main(String[] args){
 		GitiUp esket = new GitiUp();
 		System.out.println("Giddy up!");
 		System.out.println("I don't know how to ride horses! D: - Jason");
+        System.out.println("cube");
 		System.out.print("zeee gaga - Lionel ~ ");
 		esket.xd();
 	}
