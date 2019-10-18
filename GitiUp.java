ecb21482ff60dd979c4a909355f015f7c7308474
LionelSequeira <lionelsequeira@gmail.com>
2019-10-18 15:54:45 -0400

    Lionel's Edit

--- GitiUp.java
+++ GitiUp.java
@@ -1,6 +1,16 @@
 public class GitiUp {
+	public void xd(){
+		int lmao;
+		for(lmao = 4; lmao > 0; lmao--){
+			System.out.print("kek ");	
+		}System.out.println("");
+	}
+	
 	public static void main(String[] args){
+		GitiUp esket = new GitiUp();
 		System.out.println("Giddy up!");
 		System.out.println("I don't know how to ride horses! D: - Jason");
+		System.out.print("zeee gaga - Lionel ~ ");
+		esket.xd();
 	}
 }
